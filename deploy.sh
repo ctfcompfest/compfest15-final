b9e56ad99cd2fe13b0d00602d51b312623a46adb
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2023-10-07 22:00:16 +0700

    Update description chall (#20)
    * Update description
    
    * add script
    
    * update script permission
    
    * Add restart info after patching

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+rm -rf chall-*
+cp -r alice-bob-and-random-numbers chall-1
+cp -r converter chall-2
+cp -r ggez chall-3
+cp -r is_admin_true chall-4
+cp -r neozap-checker chall-5
+cp -r writeup-editor chall-6
